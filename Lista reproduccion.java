94cda4c7a0b9ae956d58c95bf4705744a741ae52
JuanCodeAI <juanito.lolgarcia215@gmail.com>
2024-11-30 19:28:51 -0500

    Create Lista reproduccion.java

---
+++ Lista reproduccion.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaReproduccion {
+    private String nombre;
+    private List<Cancion> canciones;
+
+    public ListaReproduccion(String nombre) {
+        this.nombre = nombre;
+        this.canciones = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Cancion> getCanciones() {
+        return canciones;
+    }
+
+    public void agregarCancion(Cancion cancion) {
+        canciones.add(cancion);
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
