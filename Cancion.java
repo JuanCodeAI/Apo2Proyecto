06fce37850d5e04e2300644b8c203d18f9951cce
JuanCodeAI <juanito.lolgarcia215@gmail.com>
2024-11-30 19:29:31 -0500

    Create Cancion.java

---
+++ Cancion.java
@@ -0,0 +1,30 @@
+public class Cancion {
+    private String nombre;
+    private Artista artista;
+
+    public Cancion(String nombre, Artista artista) {
+        this.nombre = nombre;
+        this.artista = artista;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - " + artista.getNombre();
+    }
+}
